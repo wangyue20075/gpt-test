fc8642a5e8b8e9dd33fb71d0ef5eeb9b50b4c810
OCEAN <dyan.le@outlook.com>
2026-04-14 10:38:32 +0800

    refactor: 使用 Flurl 重构 Binance API 请求

---
+++ src/Infrastructure/Repositories/PositionRepository.cs
@@ -0,0 +1,31 @@
+﻿using Oc.BinGrid.Domain.Entities;
+using Oc.BinGrid.Domain.Enums;
+using Oc.BinGrid.Domain.Interfaces;
+using SqlSugar;
+using Volo.Abp.DependencyInjection;
+
+namespace Oc.BinGrid.Infrastructure.Repositories
+{
+    public class PositionRepository : IPositionRepository, ITransientDependency
+    {
+        private readonly ISqlSugarClient _db;
+
+        public PositionRepository(ISqlSugarClient db)
+        {
+            _db = db;
+        }
+
+        public async Task UpdateAsync(EaPosition position)
+        {
+            var storage = _db.Storageable(new List<EaPosition> { position }).ToStorage();
+            await storage.AsInsertable.ExecuteCommandAsync();
+            await storage.AsUpdateable.ExecuteCommandAsync();
+        }
+
+        public Task<EaPosition?> GetPositionAsync(string strategyName, string symbol, PositionSide side)
+        {
+            return _db.Queryable<EaPosition>()
+                .FirstAsync(x => x.StrategyName == strategyName && x.Symbol == symbol && x.Side == side);
+        }
+    }
+}
